8cfbfdcc2996095921b1865cec61bab42918c67b
thiru <thiru.t@mavens-i.com>
2026-06-23 14:38:37 +0530

    push key

--- js/config.js
+++ js/config.js
@@ -5,5 +5,5 @@
    =================================================================== */
 window.KPT_CONFIG = {
   // e.g. "https://script.google.com/macros/s/AKfy....../exec"
-  EVENTS_API: "https://script.google.com/macros/s/AKfycbz_hwloT9KWuqdB-AhC6hmgSfGC4It2C5H1Z-TDkl95gxEVYyQ3VkjPx1Vk8uE9wb8w/exec"
+  EVENTS_API: "https://script.google.com/macros/s/AKfycbycKD4sfiuyeLiE1BmqwjQuudZQlEt6pin2fzqZTu_5tjpn90WpO9zh2y54hXfPbs7E/exec"
 };
